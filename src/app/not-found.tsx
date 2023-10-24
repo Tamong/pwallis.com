dd732aaf5d5ade0ffcf11f6e394dfc7ee08ae0a8
Philip Wallis <philiptw115@gmail.com>
2023-10-24 17:07:06 -0500

    add redirect button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,24 @@
-import Head from "next/head";
+import { MoveLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function NotFoundPage() {
   return (
     <>
-      <Head>
-        <title>404</title>
-        <meta name="description" content="404" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta http-equiv="refresh" content="5; URL='/'" />
-      </Head>
-      <h1 className="flex h-[100dvh] items-center justify-center text-center text-2xl">
-        What are you doing...
-      </h1>
+      <div
+        className="flex h-[100dvh] flex-col items-center justify-center
+        text-center"
+      >
+        <h1 className="text-2xl">What are you doing...</h1>
+        <Button variant="link">
+          <Link href="/">
+            <h2 className="flex items-center gap-2 text-xl">
+              <MoveLeft />
+              Go Home
+            </h2>
+          </Link>
+        </Button>
+      </div>
     </>
   );
 }
