8c908a73d21988801e50f0a026fd786207bec057
Philip Wallis <philiptw115@gmail.com>
2023-10-31 22:39:08 -0500

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,19 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
+  metadataBase: new URL("https://pwallis.com"),
+
+  title: "Philip Wallis",
+  description:
+    "A student at UTD, graduating Winter of 2023. Currently exploring job opportunities.",
+
+  icons: [{ rel: "icon", url: "/leaf.svg" }],
   openGraph: {
     title: "Philip Wallis",
     description:
       "A student at UTD, graduating Winter of 2023. Currently exploring job opportunities.",
     url: "https://pwallis.com",
     site_name: "Philip Wallis",
-    icons: [{ rel: "icon", url: "/leaf.svg" }],
     images: [{ url: "/thumbnail.png", alt: "Philip Wallis" }],
     locale: "en_US",
     type: "website",
